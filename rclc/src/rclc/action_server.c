4c945945d152423a15b7361a7a5a26e6fc645222
Pablo Garrido <pablogs9@gmail.com>
2020-11-04 13:08:45 +0100

    Copy client action to server action

---
+++ rclc/src/rclc/action_server.c
@@ -0,0 +1,49 @@
+// Copyright (c) 2019 - for information on the respective copyright owner
+// see the NOTICE file and/or the repository https://github.com/ros2/rclc.
+// Copyright 2014 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "rclc/action_client.h"
+
+#include <rcl/error_handling.h>
+#include <rcutils/logging_macros.h>
+
+rcl_ret_t
+rclc_action_client_init_default(
+  rcl_action_client_t * action_client,
+  const rcl_node_t * node,
+  const rosidl_action_type_support_t * type_support,
+  const char * action_name)
+{
+  
+  RCL_CHECK_FOR_NULL_WITH_MSG(
+    action_client, "action client is a null pointer", return RCL_RET_INVALID_ARGUMENT);
+  RCL_CHECK_FOR_NULL_WITH_MSG(
+    node, "node is a null pointer", return RCL_RET_INVALID_ARGUMENT);
+  RCL_CHECK_FOR_NULL_WITH_MSG(
+    type_support, "type_support is a null pointer", return RCL_RET_INVALID_ARGUMENT);
+  RCL_CHECK_FOR_NULL_WITH_MSG(
+    action_name, "action_name is a null pointer", return RCL_RET_INVALID_ARGUMENT);
+
+  rcl_action_client_options_t action_client_opt = rcl_action_client_get_default_options();
+  rcl_ret_t rc = rcl_action_client_init(
+    action_client,
+    node,
+    type_support,
+    action_name,
+    &action_client_opt);
+  if (rc != RCL_RET_OK) {
+    PRINT_RCLC_ERROR(rclc_client_init_default, rcl_client_init);
+  }
+}
